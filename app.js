2571d2b2c83902e259cc9abae7ee81f8f130213c
Lonejyc <jocelyn.marcilloux@gmail.com>
2025-01-06 16:08:46 +0100

    feat: hit and stand value added

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ import { serve } from '@hono/node-server';
 import { cors } from 'hono/cors';
 
 const datas = [
-    { uuid: '3E F4 5R 3D 2F', team: 'blue' },
-    { uuid: '5F 4R 7J 6J 7E', team: 'red' }
+    { uuid: '3E F4 5R 3D', team: 'blue', stand: '0', hit: '0' },
+    { uuid: '5F 4R 7J 6J', team: 'red', stand: '0', hit: '0' }
 ];
 
 const app = new Hono();
@@ -30,13 +30,14 @@ app.post('/api/data', async (c) => {
     const existingData = datas.find(d => d.uuid === data.uuid);
 
     if (existingData) {
-        if (existingData.team === data.team) {
-            // Remove the data if it already exists with the same team
+        if (existingData.team === data.team && existingData.hit === data.hit && existingData.stand === data.stand) {
+            // Remove the data if it already exists with the same team, hit, and stand
             datas.splice(datas.findIndex(d => d.uuid === data.uuid), 1);
         } else {
-            // Update the team if the uuid exists but with a different team
-            datas.splice(datas.findIndex(d => d.uuid === data.uuid), 1);
-            datas.push(data);
+            // Update the data if any of the fields (team, hit, or stand) change
+            existingData.team = data.team;
+            existingData.hit = data.hit;
+            existingData.stand = data.stand;
         }
     } else {
         // Add new data if the uuid does not exist
